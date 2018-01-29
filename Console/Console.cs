6ffc8c2e5c72ca4d7e0bbc1e177b989b4e2d2102
Giuliano Rosso <rossogiuliano@gmail.com>
2018-01-29 17:27:42 -0300

    Separated standard commands to another file

--- Console/Console.cs
+++ Console/Console.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace Nagma
 {
-    public class Console
+    public partial class Console
     {
         public const string CONSOLE_VER = "0.1.0";
 
@@ -130,7 +130,6 @@ namespace Nagma
             }
         }
 
-        #region Standard commands
         private void AddStandardCommands()
         {
             // Add the standard commands
@@ -139,50 +138,5 @@ namespace Nagma
             AddCommand("clear", "Clears the console's journal.", Clear);
             AddCommand("echo", "Outputs the given text.", Echo);
         }
-
-        private void ShowAllCommands(string[] args)
-        {
-            StringBuilder sb = new StringBuilder();
-
-            sb.AppendLine("\nAvailable commands:\n");
-
-            var longestCommandLength = CommandsList.Max(e => e.Key.Length);
-
-            foreach(var command in CommandsList)
-            {
-                var nameToOut = command.Key;
-
-                for (int i = command.Key.Length; i < longestCommandLength; i++)
-                    nameToOut += " ";
-
-                if (String.IsNullOrEmpty(command.Value.CommandDescription))
-                {
-                    sb.AppendLine(String.Format("{0} No description.", nameToOut));
-                }
-                else
-                {
-                    sb.AppendLine(String.Format("{0} {1}", nameToOut, command.Value.CommandDescription));
-                }
-            }
-
-            Log(sb.ToString());
-        }
-
-        private void ConsoleVersion(string[] args)
-        {
-            Log(String.Format("Current version: {0}", CONSOLE_VER));
-        }
-
-        private void Echo(string[] args)
-        {
-            Log(String.Join(" ", args.SubArray(1, args.Length - 1)));
-        }
-
-        public void Clear(string[] args)
-        {
-            Journal.Clear();
-            JournalWhiped.Invoke(new JournalWhipedEventArgs());
-        }
-        #endregion
     }
 }
---
+++ Console/StandardCommands.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nagma
+{
+    public partial class Console
+    {
+        private void ShowAllCommands(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("\nAvailable commands:\n");
+
+            var longestCommandLength = CommandsList.Max(e => e.Key.Length);
+
+            foreach (var command in CommandsList)
+            {
+                var nameToOut = command.Key;
+
+                for (int i = command.Key.Length; i < longestCommandLength; i++)
+                    nameToOut += " ";
+
+                if (String.IsNullOrEmpty(command.Value.CommandDescription))
+                {
+                    sb.AppendLine(String.Format("{0} No description.", nameToOut));
+                }
+                else
+                {
+                    sb.AppendLine(String.Format("{0} {1}", nameToOut, command.Value.CommandDescription));
+                }
+            }
+
+            Log(sb.ToString());
+        }
+
+        private void ConsoleVersion(string[] args)
+        {
+            Log(String.Format("Current version: {0}", CONSOLE_VER));
+        }
+
+        private void Echo(string[] args)
+        {
+            Log(String.Join(" ", args.SubArray(1, args.Length - 1)));
+        }
+
+        public void Clear(string[] args)
+        {
+            Journal.Clear();
+            JournalWhiped.Invoke(new JournalWhipedEventArgs());
+        }
+    }
+}
